db3c5bca0647072f2caa89d9ed72676673f1730f
RAmiHomsi <ramihomsi1997@hotmail.com>
2025-09-19 20:17:32 +0200

    finished backend structure

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import { NavLink, useNavigate } from "react-router-dom";
 const Navbar = () => {
   const navigate = useNavigate();
 
-  const [showMenu, sethowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
   const [token, setToken] = useState(true);
 
   return (
@@ -34,12 +35,19 @@ const Navbar = () => {
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
-      <div className="flex items-center">
+      <div className="flex items-center gap-4">
         {token ? (
-          <div className="flex items-center gap-2 cursor-pointer group relative">
+          <div
+            onClick={() => setShowProfile(!showProfile)}
+            className="flex items-center gap-2 cursor-pointer relative"
+          >
             <img className="w-8 rounded-full" src={assets.profile_pic} alt="" />
             <img className="w-2.5" src={assets.dropdown_icon} alt="" />
-            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+            <div
+              className={`absolute top-0 right-0 pt-14 font-medium text-gray-600 z-20 ${
+                showProfile ? "block" : "hidden"
+              }`}
+            >
               <div className="min-w-48 bg-gray-50 rounded flex flex-col gap-4 p-4">
                 <p
                   onClick={() => navigate("/my-profile")}
@@ -65,11 +73,48 @@ const Navbar = () => {
         ) : (
           <button
             onClick={() => navigate("/login")}
-            className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer"
+            className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block"
           >
-            Create Account
+            Create account
           </button>
         )}
+
+        {/* ---- Mobile Menu ---- */}
+        <img
+          onClick={() => setShowMenu(true)}
+          className="w-6 md:hidden"
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div
+          className={`md:hidden ${
+            showMenu ? "fixed w-full" : "h-0 w-0"
+          } right-0 top-0 z-20 overflow-hidden bg-white transition-all`}
+        >
+          <div className="flex items-center justify-between px-5 py-6">
+            <img src={assets.logo} className="w-30" alt="" />
+            <img
+              onClick={() => setShowMenu(false)}
+              src={assets.cross_icon}
+              className="w-7"
+              alt=""
+            />
+          </div>
+          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink onClick={() => setShowMenu(false)} to="/">
+              <p className="px-4 py-2 rounded full inline-block">HOME</p>
+            </NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to="/doctors">
+              <p className="px-4 py-2 rounded full inline-block">ALL DOCTORS</p>
+            </NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to="/about">
+              <p className="px-4 py-2 rounded full inline-block">ABOUT</p>
+            </NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to="/contact">
+              <p className="px-4 py-2 rounded full inline-block">CONTACT</p>
+            </NavLink>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -26,9 +26,17 @@ const Doctors = () => {
     <div>
       <p className="text-gray-600">Browse through the doctors specialist.</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
+        <button
+          onClick={() => setShowFilter(!showFilter)}
+          className={`py-1 px-3 border rounded text-sm  transition-all sm:hidden ${
+            showFilter ? "bg-primary text-white" : ""
+          }`}
+        >
+          Filters
+        </button>
         <div
-          className={`flex-col gap-4 text-sm text-gray-600 ${
-            showFilter ? "flex" : "hidden sm:flex"
+          className={`sm:flex flex-col gap-4 text-sm text-gray-600 ${
+            showFilter ? "flex" : "hidden"
           }`}
         >
           <p
