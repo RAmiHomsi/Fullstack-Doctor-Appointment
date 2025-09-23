613acfeefab711f88c9336f7c4378eafed6f7766
RAmiHomsi <ramihomsi1997@hotmail.com>
2025-09-23 15:27:01 +0200

    added login and creating a doctor APIs

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 
 // app config
 const app = express();
@@ -12,9 +13,11 @@ connectCloudinary();
 
 // middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // api endpoints
+app.use("/api/admin", adminRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
