ee76b9831349040742b6c7c4db17cec655462ea5
RAmiHomsi <ramihomsi1997@hotmail.com>
2025-10-08 17:04:38 +0200

    added doctorlist in the admin dashboard

--- backend/middleware/authAdmin.js
+++ backend/middleware/authAdmin.js
@@ -3,14 +3,14 @@ import jwt from "jsonwebtoken";
 // admin authentication middleware
 const authAdmin = async (req, res, next) => {
   try {
-    const { token } = req.headers;
-    if (!token) {
+    const { atoken } = req.headers;
+    if (!atoken) {
       return res.json({
         success: false,
         message: "Not Authorized Login Again",
       });
     }
-    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+    const token_decode = jwt.verify(atoken, process.env.JWT_SECRET);
     if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
       return res.json({
         success: false,
