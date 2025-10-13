bfe47be3a8b6416027fc0a2d12dc27ef668dc202
RAmiHomsi <ramihomsi1997@hotmail.com>
2025-10-13 13:20:58 +0200

    created the backend for login and register functionalities for a normal user

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,13 +1,47 @@
-import { createContext } from "react";
-import { doctors } from "../assets/assets";
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "$";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const [doctors, setDoctors] = useState([]);
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
+  const [userData, setUserData] = useState(false);
+
+  // Getting Doctors using API
+  const getDoctosData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list");
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getDoctosData();
+  }, []);
+
   const value = {
     doctors,
+    getDoctosData,
     currencySymbol,
+    backendUrl,
+    token,
+    setToken,
+    userData,
+    setUserData,
   };
 
   return (
